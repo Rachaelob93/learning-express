9aa7b8f39324f10738bfc4049d8e347ae9972f0f
Rachael O'Brien <rachaelob93@gmail.com>
2021-01-28 15:52:35 +0000

    middleware examples and public folder added

--- src/server.js
+++ src/server.js
@@ -1,48 +1,77 @@
 const express = require("express");
-const app = express();
-app.use(express.json());
+const fs = require("fs")
+const path = require("path");
 
-app.get("/", (req, res)=> {
-    console.log("Logging something cool");
+const app = express();
+const public_directory = path.join(__dirname, "../public")
+
+app.use(express.json());
+app.use(express.static(public_directory));
+
+//MIDDLEWARE EXAMPLE
+const myFunction = (req, res, next) => {
+    console.log("This happens first");
+    req.query.random = "something random"
+
+    next();
+}
+
+const myOtherFuncion = (req, res, next) => {
+    console.log("this happens second")
+    req.query.morestuff = "something else";
+    next();
+}
+
+// MYFUNCTION MIDDLEWARE BEING RUN
+app.get("/", [myFunction, myOtherFuncion], (req, res)=> {
+    console.log(".... then running controller");
+    console.log(req.query);
     res.send("Hello world");
 });
 
-app.get("/data", (req, res)=>{
+// app.get("/data", (req, res)=>{
 
-    console.log(req.query)
+//     console.log(req.query)
     
-    res.send({data: "Hello Dan"});
-});
+//     res.send({data: "Hello Dan"});
+// });
 
-app.get("/me", (req, res)=>{
+// app.get("/me", (req, res)=>{
     
-    res.send({name: "Rachael", age:"27"});
-});
+//     res.send({name: "Rachael", age:"27"});
+// });
 
-app.get("/about", (req, res)=>{
+// app.get("/about", (req, res)=>{
     
-    res.send("YOYOYO");
+//     res.send("YOYOYO");
+// });
+
+// app.get("/dog/:id", (req, res)=> {
+//     console.log(req.params);
+//     res.send("success");
+// });
+
+// app.get("/person/:id", (req, res)=> {
+//     console.log(req.params.id);
+//     res.send("success");
+// });
+
+// app.post("/", (req, res) => {
+//     console.log(req.body);
+//     res.send({data: req.body })
+// });
+
+// app.post("/practice", (req,res)=> {
+//     console.log(req.body);
+//     res.send({data: req.body})
+// });
+
+app.post("/task", (req, res) => {
+    fs.writeFileSync("tasks.txt", req.body.task);
+    res.send("Success")
 });
 
-app.get("/dog/:id", (req, res)=> {
-    console.log(req.params);
-    res.send("success");
-});
 
-app.get("/person/:id", (req, res)=> {
-    console.log(req.params.id);
-    res.send("success");
-});
-
-app.post("/", (req, res) => {
-    console.log(req.body);
-    res.send({data: req.body })
-});
-
-app.post("/practice", (req,res)=> {
-    console.log(req.body);
-    res.send({data: req.body})
-})
 
 
 app.listen(5000, ()=> {
